d44a0d8e77527bedec6ed1b1fff45bf79b2a4d53
pramod231 <conniviatech333@gmail.com>
2022-10-21 13:33:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/locationcamera/MainActivity.kt
@@ -0,0 +1,434 @@
+package com.example.locationcamera
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    val PERMISSION_REQUEST_ACCESS_LOCATION = 100
+
+    // location on camera
+    private lateinit var mapFragment: SupportMapFragment
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var tvAddressTitle: TextView
+    private lateinit var tvFullAddress: TextView
+    private lateinit var tvLat: TextView
+    private lateinit var tvLong: TextView
+    private lateinit var tvDate: TextView
+    private lateinit var llDisplayAddress: LinearLayout
+    val rotate = 0
+    private lateinit var mLastLocation : Location
+
+//     lateinit var llDisplayAddress: LinearLayout
+
+
+    var addresses: List<Address>? = null
+    private lateinit var  geocoder: Geocoder
+    private lateinit var  mMap: GoogleMap
+
+    private var village: String = ""
+    private var state: String = ""
+    private var district: String = ""
+    private var country: String = ""
+    private var area: String = ""
+    private var lati: Double = 0.0
+    private var longi: Double = 0.0
+
+    private var utils : Utils = Utils()
+    private  var locationBitmap : Bitmap? = null
+    private  var bitmap : ImageCapture? = null
+    private  var new : Bitmap? = null
+
+
+//location on camera
+
+    private var imageCapture: ImageCapture? = null
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //        Initialization
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        tvAddressTitle = findViewById(R.id.tvAddressTitle)
+        tvFullAddress = findViewById(R.id.tvFullAddress)
+        tvLat = findViewById(R.id.tvLat)
+        tvLong = findViewById(R.id.tvLong)
+        tvDate = findViewById(R.id.tvDate)
+        llDisplayAddress = findViewById(R.id.llDisplayAddress)
+//        llDisplayAddress = findViewById(R.id.llDisplayAddress)
+
+        //Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mapFragment = supportFragmentManager.findFragmentById(R.id.googleMapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+
+
+        geocoder = Geocoder(this@MainActivity)
+        Log.i("ADDRESS","before getting loc is ${tvFullAddress.text}")
+        getCurrentLocation()
+
+        Log.i("ADDRESS","after got loc is ${tvFullAddress.text}")
+//        bitmap = utils.createBitmapFromLayout(llDisplayAddress)
+
+
+//        utils.saveToGallery(this, locationBitmap!!,"DCIM")
+
+//
+
+
+
+
+
+//        Initialization
+
+        // hide the action bar
+        supportActionBar?.hide()
+
+        // Check camera permissions if all permission granted
+        // start camera else ask for the permission
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        // set on click listener for the button of capture photo
+        // it calls a method which is implemented below
+
+        findViewById<Button>(R.id.camera_capture_button).setOnClickListener {
+            takePhoto()
+            new?.let { it1 -> utils.saveToGallery(this, it1,"DCIM") }
+//            bitmap?.let { it1 -> utils.saveToGallery(this, it1,"DCIM") }
+
+        }
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun takePhoto() {
+        // Get a stable reference of the
+        // modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+        bitmap = imageCapture
+
+        // Create time-stamped output file to hold the image
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis()) + ".jpg"
+        )
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        // Set up image capture listener,
+        // which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+
+                    runOnUiThread {
+                        val yo: Bitmap = BitmapFactory.decodeFile(photoFile.path)
+
+
+//                       new  =  utils.overlay(yo,locationBitmap!!)
+//                       new  =  utils.bitmapOverlayToCenter(yo,locationBitmap!!)
+                     val   newLocation = utils.getResizedBitmap(locationBitmap!!, locationBitmap!!.width*3,locationBitmap!!.height*3)
+//                        val resizedLocation = utils.getResizedBitmap(newLocation!!,100,50)!!
+                        new = utils.mark(yo,newLocation!!)
+                        Log.i("NOOOW","yo i s$yo")
+                        Log.i("NOOOW","loco i s$locationBitmap")
+                        Log.i("NOOOW","new i s$new")
+                        Log.i("ADDRESS","after captured in getLoc fun i s${tvFullAddress.text}")
+
+                                utils.saveToGallery(applicationContext,new!!,"DCIM")
+                        Toast.makeText(this@MainActivity, "Image saved!", Toast.LENGTH_LONG).show()
+                    }
+
+                    // set the saved uri to the image view
+//                    findViewById<ImageView>(R.id.iv_capture).visibility = View.VISIBLE
+//                    findViewById<ImageView>(R.id.iv_capture).setImageURI(savedUri)
+
+                    val msg = "Photo capture succeeded: $savedUri"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_LONG).show()
+                    Log.d(TAG, msg)
+                }
+            })
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewFinder.createSurfaceProvider())
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture
+                )
+
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    // creates a folder inside internal storage
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    // checks the camera permission
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            // If all permissions granted , then start Camera
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                // If permissions are not granted,
+                // present a toast to notify the user that
+                // the permissions were not granted.
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT)
+                    .show()
+                finish()
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "CameraXGFG"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 20
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    private fun getCurrentLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            requestPermission()
+        }
+
+        val task = fusedLocationProviderClient.lastLocation
+        task.addOnSuccessListener { location ->
+            if (location != null) {
+
+                mapFragment.getMapAsync(OnMapReadyCallback {
+                    var latLng = LatLng(location.latitude, location.longitude)
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 100f))
+
+
+
+                    lati = latLng.latitude
+                    longi = latLng.longitude
+
+                    addresses = geocoder.getFromLocation(lati, longi, 1)
+                    state = addresses!![0].adminArea
+                    district = addresses!![0].locality
+                    country = addresses!![0].countryName
+                    area =  addresses!![0].getAddressLine(0)
+
+                    val sdf = SimpleDateFormat("dd/M/yyyy")
+                    val currentDate = sdf.format(Date())
+
+
+
+                    tvDate.setText(currentDate)
+                    tvFullAddress.setText(area)
+                    tvAddressTitle.setText("$district, $state, $country")
+                    tvLat.setText("Lat ${lati}")
+                    tvLong.setText("Lat ${longi}")
+
+                    Log.i("ADDRESS","adreess in getLoc fun i s${tvFullAddress.text}")
+                    Log.e("getCurrentLocation", latLng.latitude.toString() + "-" + latLng.longitude)
+                })
+
+            }
+
+
+
+        }
+
+        locationBitmap = utils.createBitmapFromLayout(llDisplayAddress)
+
+
+
+
+
+
+//
+//        fusedLocationProviderClient.lastLocation.addOnCompleteListener(this) { task ->
+//
+//            val location: Location? = task.result
+//            if (location == null) {
+//                Toast.makeText(this, "Failed to access location", Toast.LENGTH_LONG).show()
+//
+//              requestPermission()
+//
+//            } else {
+//
+//
+//
+//                val lat = location.latitude
+//                val long = location.longitude
+//
+//                lati = location.latitude
+//                longi = location.longitude
+//
+//                addresses = geocoder.getFromLocation(lati,longi,1)
+//                Log.i("OLDI","Lati is $lati & Longi is $longi")
+//                Log.i("OLDI","Lat is $lat & long is $long")
+//
+//                if ( lat.toInt() != 0 && long.toInt() != 0){
+//                    addresses = geocoder.getFromLocation(lat.toDouble(), long.toDouble(), 1)
+//                    state = addresses!![0].adminArea
+//                    district = addresses!![0].locality
+//                    country = addresses!![0].countryName
+//                    area =  addresses!![0].getAddressLine(0)
+//
+//                    val current = LocalDateTime.now()
+//                    val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+//                    val formatted = current.format(formatter)
+//
+//
+//                tvDate.text = formatted
+//                    tvFullAddress.text = area
+//                    tvAddressTitle.text = "$district, $state, $country"
+//                    tvLat.text = "Lat ${location.latitude}"
+//                    tvLong.text = "Lat ${location.longitude}"
+//                }
+//
+//
+//
+//
+//            }
+//        }
+
+
+    }
+
+    private fun requestPermission() {
+
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION),
+            PERMISSION_REQUEST_ACCESS_LOCATION
+        )
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        mMap = googleMap
+        mMap.uiSettings.isMapToolbarEnabled = true
+        mMap.uiSettings.setAllGesturesEnabled(true)
+        mMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        mMap.isMyLocationEnabled = true
+
+
+
+        val currentLocation = LatLng(lati, longi)
+        Log.i("LATI", "latlong is $lati & Longi is $longi")
+
+        mMap.addMarker(MarkerOptions().position(currentLocation).visible(true))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 50f))
+        mMap.moveCamera(CameraUpdateFactory.zoomIn())
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(50f), 2000, null)
+
+    }
+
+    override fun onLocationChanged(location: Location) {
+        mLastLocation = location
+//Place current location marker
+        val latLng = LatLng(location.latitude, location.longitude)
+        Log.e("onLocationChanged", latLng.latitude.toString() + "-" + latLng.longitude)
+
+//move map camera
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+        mMap.moveCamera(CameraUpdateFactory.zoomIn())
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(18f))
+    }
+
+}
---
+++ app/src/main/java/com/example/locationcamera/Utils.kt
@@ -0,0 +1,254 @@
+package com.example.locationcamera
+
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.*
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import androidx.camera.core.ImageProxy
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+import java.nio.ByteBuffer
+import java.util.*
+
+
+class Utils {
+    fun createBitmapFromLayout(tv: View): Bitmap {
+        val spec: Int = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+        tv.measure(spec, spec)
+        tv.layout(10, 10, tv.measuredWidth, tv.measuredHeight)
+        val b = Bitmap.createBitmap(
+            tv.measuredWidth, tv.measuredWidth,
+            Bitmap.Config.ARGB_8888
+        )
+        val c = Canvas(b)
+        c.translate((-tv.scrollX).toFloat(), (-tv.scrollY).toFloat())
+        tv.draw(c)
+        return b
+    }
+
+    fun overlay(bmp1: Bitmap, bmp2: Bitmap): Bitmap? {
+        val bmOverlay = Bitmap.createBitmap(bmp1.width, bmp1.height, bmp1.config)
+        val canvas = Canvas(bmOverlay)
+        canvas.drawBitmap(bmp1, Matrix(), null)
+        canvas.drawBitmap(bmp2, 0f, 0f, null)
+        bmp1.recycle()
+        bmp2.recycle()
+        return bmOverlay
+    }
+
+
+
+    fun getImageUri( inImage: Bitmap): Uri {
+
+        val tempFile = File.createTempFile("temprentpk", ".png")
+        val bytes = ByteArrayOutputStream()
+        inImage.compress(Bitmap.CompressFormat.PNG, 100, bytes)
+        val bitmapData = bytes.toByteArray()
+
+        val fileOutPut = FileOutputStream(tempFile)
+        fileOutPut.write(bitmapData)
+        fileOutPut.flush()
+        fileOutPut.close()
+        return Uri.fromFile(tempFile)
+    }
+
+    fun saveImage(finalBitmap: Bitmap, image_name: String) {
+        val root = Environment.getExternalStorageDirectory().toString()
+        val myDir = File(root)
+        myDir.mkdirs()
+        val fname = "Image-$image_name.jpg"
+        val file = File(myDir, fname)
+        if (file.exists()) file.delete()
+        Log.i("LOAD", root + fname)
+        try {
+            val out = FileOutputStream(file)
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out)
+            out.flush()
+            out.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun bitmapToByte (bitmap: Bitmap) : ByteArray{
+        val width = bitmap.getWidth()
+        val height = bitmap.getHeight()
+
+        val size: Int = bitmap.getRowBytes() * bitmap.getHeight()
+        val byteBuffer: ByteBuffer = ByteBuffer.allocate(size)
+        bitmap.copyPixelsToBuffer(byteBuffer)
+
+        var byteArray = byteBuffer.array()
+        return byteArray
+    }
+
+    fun byteToUri(yo : ByteArray) : Uri {
+        val buf :ByteArray = yo
+        val s  =  "$buf" + "," + "UTF-8"
+        val uri : Uri = Uri.parse(s)
+        return uri
+    }
+
+    fun SaveImage(finalBitmap: Bitmap) {
+
+        val root = Environment.getExternalStorageDirectory().toString()
+        val myDir = File(root + "/testappimages")
+        myDir.mkdirs()
+        val generator = Random()
+        var n = 10000
+        n = generator.nextInt(n)
+        val OutletFname = "Image-$n.jpg"
+        val file = File(myDir, OutletFname)
+        if (file.exists()) file.delete()
+        try {
+            val out = FileOutputStream(file)
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out)
+//            imagePath = file.absolutePath
+            out.flush()
+            out.close()
+
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+
+        }
+
+    }
+
+    fun saveToGallery(context: Context, bitmap: Bitmap, albumName: String) {
+        val filename = "${System.currentTimeMillis()}.png"
+        val write: (OutputStream) -> Boolean = {
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, it)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val contentValues = ContentValues().apply {
+                put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                put(MediaStore.MediaColumns.MIME_TYPE, "image/png")
+                put(MediaStore.MediaColumns.RELATIVE_PATH, "${Environment.DIRECTORY_DCIM}/$albumName")
+            }
+
+            context.contentResolver.let {
+                it.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)?.let { uri ->
+                    it.openOutputStream(uri)?.let(write)
+                }
+            }
+        } else {
+            val imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString() + File.separator + albumName
+            val file = File(imagesDir)
+            if (!file.exists()) {
+                file.mkdir()
+            }
+            val image = File(imagesDir, filename)
+            write(FileOutputStream(image))
+        }
+    }
+
+    private fun imageProxyToBitmap(image: ImageProxy): Bitmap {
+        val planeProxy = image.planes[0]
+        val buffer: ByteBuffer = planeProxy.buffer
+        val bytes = ByteArray(buffer.remaining())
+        buffer.get(bytes)
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+    }
+
+
+    fun bitmapOverlayToCenter(bitmap1: Bitmap, overlayBitmap: Bitmap): Bitmap? {
+        val bitmap1Width = bitmap1.width
+        val bitmap1Height = bitmap1.height
+        val bitmap2Width = overlayBitmap.width
+        val bitmap2Height = overlayBitmap.height
+        val marginLeft = (bitmap1Width * 0.5 - bitmap2Width * 0.5).toFloat()
+        val marginTop = (bitmap1Height * 0.5 - bitmap2Height * 0.5).toFloat()
+        val finalBitmap = Bitmap.createBitmap(bitmap1Width, bitmap1Height, bitmap1.config)
+        val canvas = Canvas(finalBitmap)
+        canvas.drawBitmap(bitmap1, Matrix(), null)
+        canvas.drawBitmap(overlayBitmap, marginLeft, marginTop, null)
+        return finalBitmap
+    }
+
+//NOt working
+     fun mergeBitmap(src: Bitmap?, watermark: Bitmap): Bitmap? {
+        if (src == null) {
+            return null
+        }
+        val w = src.width
+        val h = src.height
+        val newb = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        val cv = Canvas(newb)
+
+        // draw src into canvas
+        cv.drawBitmap(src, 0f, 0f, null)
+
+        // draw watermark into
+        cv.drawBitmap(watermark, null, Rect(9, 25, 154, 245), null)
+
+        // save all clip
+        cv.save()
+
+        // store
+        cv.restore()
+        return newb
+    }
+
+
+     fun mark(src: Bitmap, watermark: Bitmap): Bitmap {
+        val w = src.width
+        val h = src.height
+
+        val result = Bitmap.createBitmap(w, h, src.config)
+        val canvas = Canvas(result)
+        canvas.drawBitmap(src, 0f, 0f, null)
+        val paint = Paint()
+        paint.color = Color.RED
+        paint.textSize = 10f
+        paint.isAntiAlias = true
+        paint.isUnderlineText = true
+//       val yo : Bitmap? =   watermark.getResizedBitmap(watermark,watermark.width*20,watermark.height*20)
+        canvas.drawBitmap(watermark, 10f, 2300f, paint)
+        return result
+    }
+
+    fun getResizedBitmap(bm: Bitmap, newWidth: Int, newHeight: Int): Bitmap? {
+        val width = bm.width
+        val height = bm.height
+        val scaleWidth = newWidth.toFloat() / width
+        val scaleHeight = newHeight.toFloat() / height
+        // CREATE A MATRIX FOR THE MANIPULATION
+        val matrix = Matrix()
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight)
+
+        // "RECREATE" THE NEW BITMAP
+        val resizedBitmap = Bitmap.createBitmap(
+            bm, 0, 0, width, height, matrix, false
+        )
+        bm.recycle()
+        return resizedBitmap
+    }
+    fun Bitmap.getResizedBitmap(bm: Bitmap, newWidth: Int, newHeight: Int): Bitmap? {
+        val width = bm.width
+        val height = bm.height
+        val scaleWidth = newWidth.toFloat() / width
+        val scaleHeight = newHeight.toFloat() / height
+        // CREATE A MATRIX FOR THE MANIPULATION
+        val matrix = Matrix()
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight)
+
+        // "RECREATE" THE NEW BITMAP
+        val resizedBitmap = Bitmap.createBitmap(
+            bm, 0, 0, width, height, matrix, false
+        )
+        bm.recycle()
+        return resizedBitmap
+    }
+}
+
